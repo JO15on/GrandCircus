eead9eb29729b2cf374fbf3ed261f990f6216afc
JO15on <JOlson1530@hotmail.com>
2020-02-25 22:45:49 -0500

    Angular Group Project

---
+++ Angular_Group_Project_API/movies-api/src/app/app.module.ts
@@ -0,0 +1,32 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { SearchCriteriaComponent } from './search-criteria/search-criteria.component';
+import { MovieListComponent } from './movie-list/movie-list.component';
+import { WatchlistPageComponent } from './watchlist-page/watchlist-page.component';
+import { HttpClientModule } from '@angular/common/http';
+import { OverviewComponent } from './overview/overview.component';
+import { HomeComponent } from './home/home.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    SearchCriteriaComponent,
+    MovieListComponent,
+    WatchlistPageComponent,
+    OverviewComponent,
+    HomeComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
