f399a89c8171830a0debb08e66f137d3c15a5d10
JO15on <JOlson1530@hotmail.com>
2020-03-27 13:37:50 -0400

    Delete movies.ts

--- Angular_Group_Project_API/src/app/movies.ts
+++
@@ -1,9 +0,0 @@
-export interface Movies {
-    data : {
-        title : string,
-        vote_count : number,
-        popularity : number, 
-        poster_path : string,
-        original_language : string
-    } [];
-}
