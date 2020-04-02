a63cdf5d34fdbfcc906d86a3dc35786643adba84
JO15on <JOlson1530@hotmail.com>
2020-04-02 12:23:51 -0400

    Create movie-list.component.ts

---
+++ Angular_Group_Project_API/src/app/movie-list/movie-list.component.ts
@@ -0,0 +1,32 @@
+import { Component, OnInit, Input,} from '@angular/core';
+import { Movies } from '../movies';
+import { MoviesService } from '../movies.service'
+
+@Component({
+  selector: 'app-movie-list',
+  templateUrl: './movie-list.component.html',
+  styleUrls: ['./movie-list.component.css']
+})
+export class MovieListComponent implements OnInit {
+
+  @Input()
+  posts : Movies;
+
+  @Input()
+  movies : Movies[] =[];
+
+  @Input()
+  open : boolean = false;
+  
+
+  constructor(public service: MoviesService) { }
+
+  ngOnInit(): void {
+    this.service.getMovies();
+  }
+
+  addMovie(post){
+    this.service.addToWatchList(post)
+  }
+
+}
